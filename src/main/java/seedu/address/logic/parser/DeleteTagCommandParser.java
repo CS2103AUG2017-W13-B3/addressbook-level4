db8a692ae7c4640d021ded75c489bd3392f0ac71
Alexander Lee <alexleegs@gmail.com>
2017-10-22 15:50:39 +0800

    Enhanced deletetag to work on single contacts

--- src/main/java/seedu/address/logic/parser/DeleteTagCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteTagCommandParser.java
@@ -1,7 +1,9 @@
 package seedu.address.logic.parser;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.DeleteTagCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -10,7 +12,7 @@ import seedu.address.model.tag.Tag;
 /**
  * Parses input arguments and creates a new DeleteTagCommand object
  */
-public class DeleteTagCommandParser {
+public class DeleteTagCommandParser implements Parser<DeleteTagCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the DeleteTagCommand
@@ -18,13 +20,31 @@ public class DeleteTagCommandParser {
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteTagCommand parse(String args) throws ParseException {
-        try {
-            Tag tag = new Tag(args);
-            return new DeleteTagCommand(tag);
-        } catch (IllegalValueException ive) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTagCommand.MESSAGE_USAGE));
+        requireNonNull(args);
+        String trimmedArgs = args.trim();
+        String[] parts = trimmedArgs.split("\\s+", 2);
+        Index index;
+        if (parts.length < 2) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTagCommand.MESSAGE_USAGE));
         }
+
+        if (parts[0].toLowerCase().equals("all")) {
+            try {
+                Tag tag = new Tag(parts[1]);
+                return new DeleteTagCommand(tag);
+            } catch (IllegalValueException ive) {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTagCommand.MESSAGE_USAGE));
+            }
+        } else {
+            try {
+                index = ParserUtil.parseIndex(parts[0]);
+                Tag tag = new Tag(parts[1]);
+                return new DeleteTagCommand(index, tag);
+            } catch (IllegalValueException ive) {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTagCommand.MESSAGE_USAGE));
+            }
+        }
+
     }
 
 }
