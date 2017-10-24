20d4b04afd2808d19f7da5fef926c39ed3785b67
Alexander Lee <alexleegs@gmail.com>
2017-10-24 16:44:05 +0800

    Updated parser

--- src/main/java/seedu/address/logic/parser/DeleteTagCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteTagCommandParser.java
@@ -20,7 +20,9 @@ public class DeleteTagCommandParser implements Parser<DeleteTagCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteTagCommand parse(String args) throws ParseException {
-        requireNonNull(args);
+        if (args == null) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTagCommand.MESSAGE_USAGE));
+        }
         String trimmedArgs = args.trim();
         String[] parts = trimmedArgs.split("\\s+", 2);
         Index index;
